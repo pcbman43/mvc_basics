ceaf4287a0c7350057a80b759fa6d9984408eaff
MKougia <kougia.mihkel@khk.ee>
2022-01-30 14:32:35 +0200

    Created project basic view

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 class Model {
-	constructor() {}
+	constructor() {
+		// data
+		this.tasks = [
+			{id:1, text: 'Be good', complete: false},
+			{id:2, text: 'Be nice', complete: false}
+		]
+	}
 }
 
 class View {
-	constructor() {}
+	constructor() {
+		// basic view
+		// root element
+		this.app = this.getElement('#root')
+		// title
+		this.title = this.setElement('h1')
+		this.title.textContent = 'Tasks'
+		// task list
+		this.taskList = this.setElement('ul')
+		// append title and task list to app
+		this.app.append(this.title, this.taskList)
+	}
+
+	getElement(selector){
+		const element = document.querySelector(selector)
+		return element
+	}
+
+	setElement(tag, classname){
+		const element = document.createElement(tag)
+		if(classname !== undefined){
+			element.classList.add(classname)
+		}
+		return element
+	}
 }
 
-class COntroller {
+class Controller {
 	constructor(model, view) {
 		this.model = model
 		this.view = view
